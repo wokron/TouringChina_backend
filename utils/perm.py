7d97ae6c7d5689509172b98db63f192a2f3664f7
wokron <3160114036@qq.com>
2023-05-03 16:09:09 +0800

    permission check decorator finish

---
+++ utils/perm.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import jwt
+from django.conf import settings
+from django.contrib.auth.models import User
+from jwt import DecodeError
+
+from utils import json
+
+
+class permission_check:
+    def __init__(self, roles: list = None):
+        self.roles = roles
+
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            request = args[-1]
+            req_jwt = request.headers.get('jwt', "")
+
+            try:
+                info = jwt.decode(req_jwt, settings.SECRET_KEY, "HS256")
+            except DecodeError as e:
+                return json.response({'result': 1, 'message': "无法解析 JWT"})
+
+            expire = datetime.fromisoformat(info['expire'])
+
+            if expire < datetime.now():
+                return json.response({'result': 1, 'message': "登陆已过期，请重新登录"})
+
+            id = info['id']
+            if not User.objects.filter(id=id).exists():
+                return json.response({'result': 1, 'message': "找不到用户"})
+            user = User.objects.get(id=id)
+
+            if not (self.roles is None or user.groups.filter(name__in=self.roles).exists()):
+                return json.response({'result': 1, 'message': "无权访问"})
+
+            return func(*args, **kwargs)
+
+        return wrapper
