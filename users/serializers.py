1a1929334fcef99f0e96ce8f4d912f5dd693c8a5
wokron <3160114036@qq.com>
2023-05-03 16:08:44 +0800

    user json serializer finish

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['name']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'groups']
